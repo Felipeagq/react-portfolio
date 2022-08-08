4388b8f7ec6d7f0a8d85d3b1d7d359b9bdc9b457
Felipeagq <felipeagq99@gmail.com>
2022-08-08 00:54:22 -0500

    [MODIFY] styles

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,28 @@
 import { Button, Icon } from "semantic-ui-react";
+import { useState } from "react";
 
 export default function Home() {
+  const [colorButton, setColorButton] = useState("primary");
+  const onHandlerClick = () => {
+    alert("Se hizo click en el Boton");
+    console.log("Se hizo click en el boton");
+    setColorButton("secondary");
+  };
   return (
     <div>
       <h1>Hola mundo NextJs</h1>
-      <Button disabled primary>Hola</Button>
-      <Button secondary>Hola</Button>
-      <Button loading={false} secondary>Hola</Button>
-      <Button loading={true} secondary>Hola</Button>
+      <Button disabled primary>
+        Hola
+      </Button>
+      <Button onClick={() => onHandlerClick()} secondary>
+        Hola
+      </Button>
+      <Button loading={false} secondary>
+        Hola
+      </Button>
+      <Button loading={true} secondary>
+        Hola
+      </Button>
       <Button animated>
         <Button.Content visible>Next</Button.Content>
         <Button.Content hidden>
